13ad3b399423b9c5b08ad0c1796045ed4f1d9430
Sergii <debug@i.ua>
2014-07-24 18:13:07 +0300

    Adding Vendor and Device Model selection into AddSendCommandForm signal selection section

--- PAK_Command_Editor/MacrosEditor/AddSendCommandForm.Designer.cs
+++ PAK_Command_Editor/MacrosEditor/AddSendCommandForm.Designer.cs
@@ -33,29 +33,35 @@
             this.cbSelectTarget = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.cbSelectSignal = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblSignal = new System.Windows.Forms.Label();
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnAddCommand = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.cbVendor = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbDeviceModel = new System.Windows.Forms.ComboBox();
+            this.lblDeviceModel = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.btnAddSignal);
-            this.groupBox1.Controls.Add(this.cbSelectTarget);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.cbSelectSignal);
-            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Location = new System.Drawing.Point(13, 13);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(341, 80);
+            this.groupBox1.Size = new System.Drawing.Size(330, 200);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Тело команды";
             // 
             // btnAddSignal
             // 
-            this.btnAddSignal.Location = new System.Drawing.Point(291, 14);
+            this.btnAddSignal.Enabled = false;
+            this.btnAddSignal.Location = new System.Drawing.Point(248, 72);
             this.btnAddSignal.Name = "btnAddSignal";
             this.btnAddSignal.Size = new System.Drawing.Size(34, 23);
             this.btnAddSignal.TabIndex = 4;
@@ -66,7 +72,7 @@
             // cbSelectTarget
             // 
             this.cbSelectTarget.FormattingEnabled = true;
-            this.cbSelectTarget.Location = new System.Drawing.Point(150, 43);
+            this.cbSelectTarget.Location = new System.Drawing.Point(107, 19);
             this.cbSelectTarget.Name = "cbSelectTarget";
             this.cbSelectTarget.Size = new System.Drawing.Size(134, 21);
             this.cbSelectTarget.TabIndex = 3;
@@ -75,33 +81,35 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 47);
+            this.label2.Location = new System.Drawing.Point(12, 23);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(135, 13);
+            this.label2.Size = new System.Drawing.Size(73, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Куда отправить (устр-во):";
+            this.label2.Text = "Устройство: ";
             // 
             // cbSelectSignal
             // 
+            this.cbSelectSignal.Enabled = false;
             this.cbSelectSignal.FormattingEnabled = true;
-            this.cbSelectSignal.Location = new System.Drawing.Point(150, 16);
+            this.cbSelectSignal.Location = new System.Drawing.Point(107, 73);
             this.cbSelectSignal.Name = "cbSelectSignal";
             this.cbSelectSignal.Size = new System.Drawing.Size(134, 21);
             this.cbSelectSignal.TabIndex = 1;
             this.cbSelectSignal.SelectedIndexChanged += new System.EventHandler(this.cb_SelectedIndexChanged);
             // 
-            // label1
+            // lblSignal
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 23);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(128, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Что отправить (сигнал):";
+            this.lblSignal.AutoSize = true;
+            this.lblSignal.Enabled = false;
+            this.lblSignal.Location = new System.Drawing.Point(12, 76);
+            this.lblSignal.Name = "lblSignal";
+            this.lblSignal.Size = new System.Drawing.Size(49, 13);
+            this.lblSignal.TabIndex = 0;
+            this.lblSignal.Text = "Сигнал: ";
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(279, 99);
+            this.btnCancel.Location = new System.Drawing.Point(268, 219);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 23);
             this.btnCancel.TabIndex = 5;
@@ -112,7 +120,7 @@
             // btnAddCommand
             // 
             this.btnAddCommand.Enabled = false;
-            this.btnAddCommand.Location = new System.Drawing.Point(198, 99);
+            this.btnAddCommand.Location = new System.Drawing.Point(187, 219);
             this.btnAddCommand.Name = "btnAddCommand";
             this.btnAddCommand.Size = new System.Drawing.Size(75, 23);
             this.btnAddCommand.TabIndex = 6;
@@ -120,11 +128,76 @@
             this.btnAddCommand.UseVisualStyleBackColor = true;
             this.btnAddCommand.Click += new System.EventHandler(this.btnAddCommand_Click);
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbDeviceModel);
+            this.groupBox2.Controls.Add(this.lblDeviceModel);
+            this.groupBox2.Controls.Add(this.cbVendor);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.cbSelectSignal);
+            this.groupBox2.Controls.Add(this.btnAddSignal);
+            this.groupBox2.Controls.Add(this.lblSignal);
+            this.groupBox2.Location = new System.Drawing.Point(19, 19);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(292, 109);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Что отправлять";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cbSelectTarget);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(19, 134);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(292, 52);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Куда отправлять";
+            // 
+            // cbVendor
+            // 
+            this.cbVendor.FormattingEnabled = true;
+            this.cbVendor.Location = new System.Drawing.Point(107, 19);
+            this.cbVendor.Name = "cbVendor";
+            this.cbVendor.Size = new System.Drawing.Size(134, 21);
+            this.cbVendor.TabIndex = 6;
+            this.cbVendor.SelectedIndexChanged += new System.EventHandler(this.cbVendor_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Производитель:";
+            // 
+            // cbDeviceModel
+            // 
+            this.cbDeviceModel.Enabled = false;
+            this.cbDeviceModel.FormattingEnabled = true;
+            this.cbDeviceModel.Location = new System.Drawing.Point(107, 46);
+            this.cbDeviceModel.Name = "cbDeviceModel";
+            this.cbDeviceModel.Size = new System.Drawing.Size(134, 21);
+            this.cbDeviceModel.TabIndex = 8;
+            this.cbDeviceModel.SelectedIndexChanged += new System.EventHandler(this.cbDeviceModel_SelectedIndexChanged);
+            // 
+            // lblDeviceModel
+            // 
+            this.lblDeviceModel.AutoSize = true;
+            this.lblDeviceModel.Enabled = false;
+            this.lblDeviceModel.Location = new System.Drawing.Point(12, 49);
+            this.lblDeviceModel.Name = "lblDeviceModel";
+            this.lblDeviceModel.Size = new System.Drawing.Size(49, 13);
+            this.lblDeviceModel.TabIndex = 7;
+            this.lblDeviceModel.Text = "Модель:";
+            // 
             // AddSendCommandForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(366, 132);
+            this.ClientSize = new System.Drawing.Size(356, 253);
             this.Controls.Add(this.btnAddCommand);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.groupBox1);
@@ -136,7 +209,10 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AddSendCommandForm_FormClosing);
             this.Load += new System.EventHandler(this.AddSendCommandForm_Load);
             this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -148,8 +224,14 @@
         private System.Windows.Forms.ComboBox cbSelectTarget;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox cbSelectSignal;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSignal;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Button btnAddCommand;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cbDeviceModel;
+        private System.Windows.Forms.Label lblDeviceModel;
+        private System.Windows.Forms.ComboBox cbVendor;
+        private System.Windows.Forms.Label label3;
     }
 }
--- PAK_Command_Editor/MacrosEditor/AddSendCommandForm.cs
+++ PAK_Command_Editor/MacrosEditor/AddSendCommandForm.cs
@@ -17,84 +17,37 @@ namespace PAK_Command_Editor.MacrosEditor
     public partial class AddSendCommandForm : Form
     {
         private ISession _dataSession;
+        private IRepository<Vendor> _vendorsRepo;
+        private IRepository<Device> _devsRepo;        
         private IRepository<Signal> _signalRepo;
         private IRepository<PredefinedGlobalVar> _pgvRepo;
         private static readonly String SELECT_TEXT = "Выберите...";
         private MacrosesEditorForm _editor;
 
+        private Vendor _currentVendor;
+        private Device _currentDevice;
+
         public AddSendCommandForm(MacrosesEditorForm editor)
         {
             InitializeComponent();
             this._dataSession = PAKDataSessionFactory.GetSession();
+            this._vendorsRepo = new Repository<Vendor>(this._dataSession);
+            this._devsRepo = new Repository<Device>(this._dataSession);
             this._signalRepo = new Repository<Signal>(this._dataSession);
             this._pgvRepo = new Repository<PredefinedGlobalVar>(this._dataSession);
             this._editor = editor;
         }
 
-        private void btnCancel_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void btnAddSignal_Click(object sender, EventArgs e)
-        {
-            AddSignalForm addSignal = new AddSignalForm();
-            addSignal.SignalAdded += addSignal_SignalAdded;
-            addSignal.ShowDialog();
-        }
-
-        void addSignal_SignalAdded(object sender, CustomEventArgs.ResultEntityEventArgs e)
-        {
-            this.BindSignals();
-        }
-
-        private void btnAddCommand_Click(object sender, EventArgs e)
-        {
-            ResultEntityEventArgs commandArgs = new ResultEntityEventArgs();
-            commandArgs.CommandType = MacrosCommandType.SEND;
-            commandArgs.Params = new List<String>() { (this.cbSelectSignal.SelectedItem as Signal).Id.ToString(), this.cbSelectTarget.SelectedItem.ToString() };
-            this._editor.BindMacroses(commandArgs);
-
-            this.Close();
-        }
+        #region AddSendCommandForm Event Handlers
 
         private void AddSendCommandForm_Load(object sender, EventArgs e)
         {
-            this.BindSignals();
-            this.BindDestinations();            
+            this.ConfigureControls();
+            this.BindVendors();
+            this.cbVendor.SelectedIndex = 0;
+
+            this.BindDestinations();
         }
-
-        private void cb_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            this.btnAddCommand.Enabled = (this.cbSelectSignal.SelectedIndex > 0) && (this.cbSelectTarget.SelectedIndex > 0);
-        }
-
-        #region Utilities
-
-        private void BindSignals()
-        {
-            this.cbSelectSignal.DisplayMember = "Name";
-            this.cbSelectSignal.ValueMember = "Id";
-
-            this.cbSelectSignal.Items.Clear();
-
-            this.cbSelectSignal.Items.Add(new { Name = SELECT_TEXT, Id = -1 });
-            this.cbSelectSignal.Items.AddRange(this._signalRepo.GetAll().ToArray());
-
-            this.cbSelectSignal.SelectedIndex = 0;
-        }
-
-        private void BindDestinations()
-        {
-            this.cbSelectTarget.Items.Clear();
-
-            this.cbSelectTarget.Items.Add(SELECT_TEXT);
-            this.cbSelectTarget.Items.AddRange(this._pgvRepo.GetAll().Select(x => x.Alias).ToArray());
-
-            this.cbSelectTarget.SelectedIndex = 0;
-        }
-
-        #endregion  
 
         private void AddSendCommandForm_FormClosing(object sender, FormClosingEventArgs e)
         {
@@ -116,5 +69,134 @@ namespace PAK_Command_Editor.MacrosEditor
                 this._dataSession.Dispose();
             }
         }
+
+        #endregion
+
+        #region Buttons Event Handlers
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAddSignal_Click(object sender, EventArgs e)
+        {
+            AddSignalForm addSignal = new AddSignalForm();
+            addSignal.SignalAdded += addSignal_SignalAdded;
+            addSignal.ShowDialog();
+        }
+
+        private void btnAddCommand_Click(object sender, EventArgs e)
+        {
+            ResultEntityEventArgs commandArgs = new ResultEntityEventArgs();
+            commandArgs.CommandType = MacrosCommandType.SEND;
+            commandArgs.Params = new List<String>() { (this.cbSelectSignal.SelectedItem as Signal).Id.ToString(), this.cbSelectTarget.SelectedItem.ToString() };
+            this._editor.BindMacroses(commandArgs);
+
+            this.Close();
+        }  
+
+        #endregion
+
+        #region ComboBoxes Event Handlers
+
+        private void cbVendor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Boolean enabled = this.cbVendor.SelectedIndex > 0;
+
+            this.lblDeviceModel.Enabled =
+                this.cbDeviceModel.Enabled = enabled;
+
+            this._currentVendor = this.cbVendor.SelectedItem as Vendor;
+
+            if (enabled)
+            {
+                this.BindDevices();
+                this.cbDeviceModel.SelectedIndex = 0;
+            }
+        }
+
+        private void cbDeviceModel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Boolean enabled = this.cbDeviceModel.SelectedIndex > 0;
+
+            this.lblSignal.Enabled =
+                this.cbSelectSignal.Enabled = this.btnAddSignal.Enabled = enabled;
+
+            this._currentDevice = this.cbDeviceModel.SelectedItem as Device;
+
+            if (enabled)
+            {
+                this.BindSignals();
+                this.cbSelectSignal.SelectedIndex = 0;
+            }
+        }
+
+        private void cb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.btnAddCommand.Enabled = (this.cbSelectSignal.SelectedIndex > 0) && (this.cbSelectTarget.SelectedIndex > 0);
+        }
+
+        #endregion
+
+        #region Related Forms Event Handlers
+
+        void addSignal_SignalAdded(object sender, CustomEventArgs.ResultEntityEventArgs e)
+        {
+            this.BindSignals();
+        } 
+
+        #endregion        
+
+        #region Utilities
+
+        private void ConfigureControls()
+        {
+            this.cbVendor.DisplayMember = "Name";
+            this.cbVendor.ValueMember = "Id";
+
+            this.cbDeviceModel.DisplayMember = "Name";
+            this.cbDeviceModel.ValueMember = "Id";
+
+            this.cbSelectSignal.DisplayMember = "Name";
+            this.cbSelectSignal.ValueMember = "Id";
+        }
+
+        private void BindVendors()
+        {
+            this.cbVendor.Items.Clear();
+            this.cbVendor.Items.Add(new { Name = SELECT_TEXT, Id = -1 });
+            this.cbVendor.Items.AddRange(this._vendorsRepo.GetAll().ToArray());
+        }
+
+        private void BindDevices()
+        {
+            this.cbDeviceModel.Items.Clear();
+            this.cbDeviceModel.Items.Add(new { Name = SELECT_TEXT, Id = -1 });
+            Device[] devices = this._devsRepo.Get(x => x.Vendor.Id == this._currentVendor.Id).ToArray();
+            this.cbDeviceModel.Items.AddRange(devices);
+        }        
+
+        private void BindSignals()
+        {
+            this.cbSelectSignal.Items.Clear();
+
+            this.cbSelectSignal.Items.Add(new { Name = SELECT_TEXT, Id = -1 });
+            this.cbSelectSignal.Items.AddRange(this._signalRepo.Get(x => x.Device.Id == this._currentDevice.Id).ToArray());
+
+            this.cbSelectSignal.SelectedIndex = 0;
+        }
+
+        private void BindDestinations()
+        {
+            this.cbSelectTarget.Items.Clear();
+
+            this.cbSelectTarget.Items.Add(SELECT_TEXT);
+            this.cbSelectTarget.Items.AddRange(this._pgvRepo.GetAll().Select(x => x.Alias).ToArray());
+
+            this.cbSelectTarget.SelectedIndex = 0;
+        }
+
+        #endregion        
     }
 }
